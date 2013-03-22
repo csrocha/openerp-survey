329dae1e33ed3e695461860589c1b2cc6c00e9ef
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2013-03-22 16:57:47 -0300

    Wait 1 seg

--- addons/survey_methodology_extra/questionnaire.py
+++ addons/survey_methodology_extra/questionnaire.py
@@ -51,7 +51,7 @@ _enter_js = """
                     currentBoxNumber = textboxes.index(e.target);
                     if (textboxes[currentBoxNumber + 1] != null) {
                             nextBox = textboxes[currentBoxNumber + 1];
-                            sleep(1000);
+                            sleep(5000);
                             nextBox.focus();
                             nextBox.select();
                             e.preventDefault();
