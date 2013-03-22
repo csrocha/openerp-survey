0518be31071bf0b44573577fb1ef66589bbdee41
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2013-03-22 15:10:09 -0300

    Different rendering for type of question

--- addons/survey_methodology_extra/questionnaire.py
+++ addons/survey_methodology_extra/questionnaire.py
@@ -358,8 +358,8 @@ class questionnaire(osv.osv):
                         )
                     if question.type=='View':
                         view_item.append(
-                            '<label string="%(complete_name)s" colspan="5"/>'
-                            '<h3 colspan="5" col="1">'
+                            '<label string="%(complete_name)s" colspan="1"/>'
+                            '<h3 colspan="4" col="1">'
                             '<label string="%(question)s"/>'
                             '</h3>'
                             '<newline/>'
