034f4faf43c96f9ce6b5ebf1641aef109da4f20e
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2013-03-22 16:53:33 -0300

    Wait 1/10 seg

--- addons/survey_methodology_extra/questionnaire.py
+++ addons/survey_methodology_extra/questionnaire.py
@@ -40,7 +40,7 @@ _enter_js = """
 	{
 		var dt = new Date();
 		dt.setTime(dt.getTime() + ms);
-		while (max(new Date().getTime(), dt.getTime()) == dt.getTime());
+		while (Math.max(new Date().getTime(), dt.getTime()) == dt.getTime());
 	};
     $(document).ready(function(){
       (function(){
