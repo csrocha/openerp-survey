dd920c510755ffab9458deff0aa0c9a5506a9224
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2013-09-04 13:13:08 -0300

    Import and update columns.

--- addons/zondaggio/wizard/questionnaire_import.py
+++ addons/zondaggio/wizard/questionnaire_import.py
@@ -39,6 +39,10 @@ class questionnaire_import(osv.osv_memory):
     _name = 'sondaggio.questionnaire_import'
     _inherit = [ _name ]
 
+    _columns = {
+        'version_column_id': fields.many2one('sondaggio.import_file_column', string='Version column'),
+    }
+
     def do_load_file(self, cr, uid, ids, context=None):
         """"""
         # Fill list of columns.
@@ -73,30 +77,47 @@ class questionnaire_import(osv.osv_memory):
         """"""
         _logger.info('Importing questionnaires.')
 
-        obj_questionnaire = self.pool.get('sondaggio.questionnaire')
+        questionnaire_obj = self.pool.get('sondaggio.questionnaire')
+        parameter_obj = self.pool.get('sondaggio.parameter')
+
         c = 0
+        u = 0
         for w in self.browse(cr, uid, ids, context=context):
             data = StringIO.StringIO(base64.b64decode(w.in_file))
             rows = csv.reader(data)
             fields = rows.next()
             column_name = w.selected_column_id.name
+            column_version = w.version_column_id.name
             survey_id = w.survey_id.id
             crc = None
             for r in rows:
                 rdict = dict(zip(fields, r))
-                m = hashlib.md5()
-                m.update(rdict[column_name])
-                dwrite = {
-                    'name': rdict[column_name],
-                    'code': m.hexdigest(),
-                    'survey_id': survey_id,
-                    'parameter_ids': [ (0,0,{ 'name': k, 'value': v }) for k, v in rdict.items() if k != column_name ],
-                }
-                obj_questionnaire.create(cr, uid, dwrite)
-                if not(c % 40): _logger.info('Created %i questionnaires and continue.' % c)
-                c = c + 1
+                questionnaire_ids = questionnaire_obj.search(cr, uid, [('name','=',rdict[column_name])])
+                if questionnaire_ids:
+                    q = questionnaire_obj.browse(cr, uid, questionnaire_ids)[0]
+                    parameters = { p.name: p.value for p in q.parameter_ids }
+                    if int(parameters.get(column_version, 0)) < int(rdict[column_version]):
+                        dwrite = {
+                            'parameter_ids': [ (0,0,{ 'name': k.lower(), 'value': v }) for k, v in rdict.items() if k != column_name ],
+                        }
+                        questionnaire_obj.write(cr, uid, [q.id], dwrite)
+                        if not(u % 40): _logger.info('Updating %i questionnaires and continue.' % u+1)
+                        u = u +1
+                else:
+                    m = hashlib.md5()
+                    m.update(rdict[column_name])
+                    dwrite = {
+                        'name': rdict[column_name],
+                        'code': m.hexdigest(),
+                        'survey_id': survey_id,
+                        'parameter_ids': [ (0,0,{ 'name': k, 'value': v }) for k, v in rdict.items() if k != column_name ],
+                    }
+                    questionnaire_obj.create(cr, uid, dwrite)
+                    if not(c % 40): _logger.info('Created %i questionnaires and continue.' % c+1)
+                    c = c + 1
 
         _logger.info('Created %i questionnaires.' % c)
+        _logger.info('Updated %i questionnaires.' % u)
 
         self.write(cr, uid, ids, {'state': 'done'}, context=context)
         return {
